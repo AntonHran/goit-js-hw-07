7f8fcd78405af430381b32bdcd14b83f9734e9e4
AntonHran <for.program.learning@gmail.com>
2024-04-25 22:53:57 +0300

    escape func

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,3 +22,36 @@ const galleryList = galleryItems.map(makeLiEl).join("");
 console.log(galleryList);
 
 ulGalleryEl.insertAdjacentHTML("beforeend", galleryList);
+
+function selectPicture(event) {
+  event.preventDefault();
+  const currentPicture = event.target;
+  if (currentPicture.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${currentPicture.dataset.source}">
+`);
+  instance.show();
+  if (instance.show()) {
+    document.addEventListener("keydown", (event) => {
+      if (event.code !== "Escape") {
+        return;
+      }
+      console.log(event.code);
+      instance.close();
+      document.removeEventListener("keydown", escapeKeyPressed);
+    });
+  }
+}
+
+ulGalleryEl.addEventListener("click", selectPicture);
+
+function escapeKeyPressed(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+  console.log(event.code);
+  document.removeEventListener("keydown", escapeKeyPressed);
+}
