597625938604ffa02cbbdaa18621da1f4cf00d72
AntonHran <for.program.learning@gmail.com>
2024-04-26 22:46:48 +0300

    edited

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulGalleryEl = document.querySelector(".gallery");
+
+function makeLiEl({ preview, original, description }) {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+}
+
+const galleryList = galleryItems.map(makeLiEl).join("");
+
+ulGalleryEl.insertAdjacentHTML("beforeend", galleryList);
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsDelay: 250,
+  captionsData: "alt",
+});
+//lightbox.on("show.simplelightbox", makeLiEl);
